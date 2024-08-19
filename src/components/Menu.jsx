c46fa6214428ba21d742902fab22b7265c5f4c61
Luiz Isaque <luizisaqueuchuu@gmail.com>
2024-08-19 18:54:33 -0300

    create animations of sections'n buttons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,42 +3,42 @@ import { Outlet, Link } from "react-router-dom";
 export function Menu() {
    return (
       <>
-         <section className="h-[94vh] w-[67vw] flex flex-col justify-between bg-base-primary backdrop-blur-sm rounded-md border-[1.1px] border-slate-300 shadow-main md:flex-row-reverse">
+         <section className="h-[94vh] w-[67vw] flex flex-col justify-between bg-base-primary backdrop-blur-sm rounded-md border-[1.1px] border-slate-300 shadow-main md:flex-row-reverse animate-contain-drop">
             <Outlet/>
             <nav className="flex justify-center items-center rounded-md w-[66.8vw] h-[5rem] bg-base-second backdrop-blur-2xl text-2xl md:rounded-r-none md:flex-col md:w-[45rem] md:h-[94vh]">
                <div className="hidden h-10 md:flex md:h-[15rem] portrait:hidden">sdfg</div>
                <ul className="flex justify-around w-full rounded-md text-mono text-skin-main md:h-full md:rounded-r-none md:flex-col md:items-center">
 
                   <Link to={'/'}>
-                     <il className="flex flex-row">
+                     <il className="flex flex-row transition ease-linear duration-75 hover:scale-125 active:animate-click-btn">
                         <i className="bi bi-house-door-fill"></i>
                         <p className="hidden lg:flex">Home</p>
                      </il>
                   </Link>
 
                   <Link to={'/About'}>
-                     <il className="flex flex-row">
+                     <il className="flex flex-row transition ease-linear duration-75 hover:scale-125 active:animate-click-btn">
                         <i className="bi bi-file-earmark-person-fill"></i>
                         <p className="hidden lg:flex">About</p>
                      </il>
                   </Link>
 
                   <Link to={'/Skill'}>
-                     <il className="flex flex-row">
+                     <il className="flex flex-row transition ease-linear duration-75 hover:scale-125 active:animate-click-btn">
                         <i className="bi bi-clipboard-check-fill"></i>
                         <p className="hidden lg:flex">Skills</p>
                      </il>
                   </Link>
                   
                   <Link to={'/Project'}>
-                     <il className="flex flex-row">
+                     <il className="flex flex-row transition ease-linear duration-75 hover:scale-125 active:animate-click-btn">
                         <i className="bi bi-suitcase-lg-fill"></i>
                         <p className="hidden lg:flex">Project</p>
                      </il>
                   </Link>
 
                   <Link to={'/Contact'}>
-                     <il className="flex flex-row">
+                     <il className="flex flex-row transition ease-linear duration-75 hover:scale-125 active:animate-click-btn">
                         <i className="bi bi-telephone-fill"></i>
                         <p className="hidden lg:flex">Contact</p>
                      </il>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,31 @@ module.exports= {
                second: 'var(--color-backgrounnd-glass2)'
             } 
          },
+         keyframes: {
+            initDrop: {
+               "0%": {
+                  transform: "translateY(17px)", opacity: "0"
+               },
+               "100%": {
+                  transform: "translateY(0px)", opacity: "1"
+               }
+            },
+
+            containDrop: {
+               "0%":  {opacity: "0"},
+               "100%":  {opacity: "1"}
+            },
+            clickBtn: {
+               "0%":  {opacity: "1"},
+               "50%":  {opacity: "0"},
+               "100%":  {opacity: "1"}
+            },
+         },
+         animation: {
+            "init-drop": "initDrop 1s ease-in-out",
+            "contain-drop": "containDrop 2s linear",
+            "click-btn": " clickBtn .2s linear",
+         }
       },
    },
    plugins: [
